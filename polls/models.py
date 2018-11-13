153f95e8add461615779eeceb2b4a3abd23afac2
azamatmurdalov <azamat.murdalov@mail.ru>
2018-11-13 17:31:11 +0300

    add test for Question model

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,6 @@
+import datetime
 from django.db import models
-
+from django.utils import timezone
 
 class Question(models.Model):
 	question_text = models.CharField(max_length=200)
@@ -7,6 +8,10 @@ class Question(models.Model):
 
 	def __str__(self):
 		return self.question_text
+
+	def was_published_recently(self):
+		now = timezone.now()
+		return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
--- polls/views.py
+++ polls/views.py
@@ -12,33 +12,22 @@ class IndexView(generic.ListView):
 	def get_queryset(self):
 		return Question.objects.order_by('-pub_date')[:5]
 
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     context = {'latest_question_list': latest_question_list}
-#     return render(request, 'polls/index.html', context)
 
 class DetailView(generic.DetailView):
 	model = Question
 	template_name = 'polls/detail.html'
 
-# def detail(request, question_id):
-# 	question = get_object_or_404(Question, pk=question_id)
-# 	return render(request, 'polls/detail.html', {'question': question})
 
 class ResultsView(generic.DetailView):
 	model = Question
 	template_name = 'polls/results.html'
 
-# def results(request, question_id):
-# 	question = get_object_or_404(Question, pk=question_id)
-# 	return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
 	try:
 		selected_choice = question.choice_set.get(pk=request.POST['choice'])
 	except (KeyError, Choice.DoesNotExist):
-		# Redisplay the question voting form.
 		return render(request, 'polls/detail.html', {
 			'question': question,
 			'error_message': "You didn't select a choice.",
@@ -46,7 +35,4 @@ def vote(request, question_id):
 	else:
 		selected_choice.votes += 1
 		selected_choice.save()
-		# Always return an HttpResponseRedirect after successfully dealin
-		# with POST data. This prevents data from being posted twice if a
-		# user hits the Back button.
 		return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
